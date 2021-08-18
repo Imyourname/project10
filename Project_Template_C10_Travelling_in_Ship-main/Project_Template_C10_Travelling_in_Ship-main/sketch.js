eac26d9b43bb35544543bc3b12b2a8d3f906624a
Imyourname <87386437+Imyourname@users.noreply.github.com>
2021-08-18 19:48:27 +0530

    Add files via upload

---
+++ Project_Template_C10_Travelling_in_Ship-main/Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -0,0 +1,25 @@
+var sea,seaimage,ship,shipimage
+function preload(){
+  seaimage=loadImage("sea.png")
+  shipimage=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea=createSprite(400,200)
+  sea.addImage(seaimage)
+  sea.velocityX=-5
+  sea.scale=0.3
+
+  ship=createSprite(140,200)
+  ship.addAnimation("moving",shipimage)
+  ship.scale=0.25
+}
+
+function draw() {
+  background("blue");
+  if(sea.x<0){
+    sea.x=sea.width/8
+  }
+ drawSprites();
+}
